21b1cc7d5cdafad87ec3230452de5b97f03216d6
Max Williams <max.williams@deliveryhero.com>
2018-11-21 10:33:28 +0100

    Small cloudwatch changes and adding new alarm for swap usage

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -3,14 +3,15 @@ locals {
 
   cloudwatch_alarm_default_thresholds = {
     "cpu_utilization" = 70
-    "freeable_memory" = 10000000
+    "freeable_memory" = 1000000000
+    "swap_usage"      = 100000000
   }
 }
 
 resource "aws_cloudwatch_metric_alarm" "cache_cpu" {
   count               = "${local.cloudwatch_create_alarms}"
-  alarm_name          = "${var.name}-${count.index + 1}-CPUUtilization"
-  alarm_description   = "${var.name} redis cluster CPU utilization"
+  alarm_name          = "${var.cloudwatch_alarm_prefix}${var.name}-${count.index + 1}-CPUUtilization"
+  alarm_description   = "ElastiCache CPU utilization for redis cluster ${var.name}"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "1"
   metric_name         = "CPUUtilization"
@@ -27,8 +28,8 @@ resource "aws_cloudwatch_metric_alarm" "cache_cpu" {
 
 resource "aws_cloudwatch_metric_alarm" "cache_memory" {
   count               = "${local.cloudwatch_create_alarms}"
-  alarm_name          = "${var.name}-${count.index + 1}-FreeableMemory"
-  alarm_description   = "${var.name} redis cluster freeable memory"
+  alarm_name          = "${var.cloudwatch_alarm_prefix}${var.name}-${count.index + 1}-FreeableMemory"
+  alarm_description   = "ElastiCache cluster freeable memory for redis cluster ${var.name}"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = "1"
   metric_name         = "FreeableMemory"
@@ -37,6 +38,25 @@ resource "aws_cloudwatch_metric_alarm" "cache_memory" {
   statistic           = "Average"
   threshold           = "${lookup(var.cloudwatch_alarm_default_thresholds, "freeable_memory", local.cloudwatch_alarm_default_thresholds["freeable_memory"])}"
   alarm_actions       = ["${var.cloudwatch_alarm_actions}"]
+
+  dimensions {
+    CacheClusterId = "${aws_elasticache_replication_group.redis.id}-00${count.index + 1}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "swap_usage" {
+  count               = "${local.cloudwatch_create_alarms}"
+  alarm_name          = "${var.cloudwatch_alarm_prefix}${var.name}-${count.index + 1}-SwapUsage"
+  alarm_description   = "ElastiCache swap usage for redis cluster ${var.name}"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "SwapUsage"
+  namespace           = "AWS/ElastiCache"
+  period              = "60"
+  statistic           = "Maximum"
+  threshold           = "${lookup(var.cloudwatch_alarm_default_thresholds, "swap_usage", local.cloudwatch_alarm_default_thresholds["swap_usage"])}"
+  alarm_actions       = ["${var.cloudwatch_alarm_actions}"]
+  ok_actions          = ["${var.cloudwatch_alarm_actions}"]
 
   dimensions {
     CacheClusterId = "${aws_elasticache_replication_group.redis.id}-00${count.index + 1}"
