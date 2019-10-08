43cbf45c3d9269b5b19e7045c23add792372c78f
oliver.klette <oliver.klette@deliveryhero.com>
2019-10-08 16:06:29 +0200

    adjust outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,13 @@
 output "id" {
   description = "The ID replication group"
-  value = element(
-    split(",", join(",", aws_elasticache_replication_group.redis.*.id)),
-    0,
-  )
+  value = concat(aws_elasticache_replication_group.redis.*.id, [""])[0]
+
 }
 
 output "security_group_id" {
   description = "The security group ID of the cluster"
-  value       = element(split(",", join(",", aws_security_group.redis.*.id)), 0)
+  value = concat(aws_security_group.redis.*.id, [""])[0]
+
 }
 
 output "port" {
@@ -18,15 +17,6 @@ output "port" {
 
 output "primary_endpoint_address" {
   description = "The primary endpoint of the cluster"
-  value = element(
-    split(
-      ",",
-      join(
-        ",",
-        aws_elasticache_replication_group.redis.*.primary_endpoint_address,
-      ),
-    ),
-    0,
-  )
+  value = concat(aws_elasticache_replication_group.redis.*.primary_endpoint_address, [""])[0]
 }
 
